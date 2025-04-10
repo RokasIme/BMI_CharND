0a80c609e3aa553f7c32d63b8c5c995ce8ad15c8
Rokas <Rokas.Imelinskas@gmail.com>
2025-04-10 16:48:05 +0300

    BMI main

---
+++ BMIND/index.js
@@ -0,0 +1,5 @@
+/* Reikalavimai:
+● KMI yra apskaičiuojamas paspaudus compute bmi mygtuką
+● Turi būti išvedamas KMI įvertis (antsvoris, normalus, viršsvoris, nutukimas)
+● Papildomai: padarykite pranešimą, jei įvestas svoris ar ūgis yra nerealistiškas (pvz neigiamas skaičius)
+*/
