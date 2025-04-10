ecfdffd488190501302928d070bf12e217de735e
Rokas <Rokas.Imelinskas@gmail.com>
2025-04-10 20:35:31 +0300

    storas

--- BMIND/index.js
+++ BMIND/index.js
@@ -3,3 +3,14 @@
 ● Turi būti išvedamas KMI įvertis (antsvoris, normalus, viršsvoris, nutukimas)
 ● Papildomai: padarykite pranešimą, jei įvestas svoris ar ūgis yra nerealistiškas (pvz neigiamas skaičius)
 */
+
+const btnDOM = document.getElementById("btn");
+const heightDOM = document.querySelector(".input");
+const weightDOM = document.getElementById("weight");
+const resultDOM = document.getElementById("bmi-result");
+const conditionDOM = document.getElementById("weight-condition");
+
+btnDOM.addEventListener("click", () => {
+  resultDOM.value = 20;
+  conditionDOM.innerText = "storas";
+});
